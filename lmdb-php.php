361fb0b0b753debf8b730087269f316edd9ab63a
mpremus <mpremus@gmail.com>
2015-04-14 14:06:26 +0200

    added logic for catching php notices

--- lmdb-php.php
+++ lmdb-php.php
@@ -1,4 +1,16 @@
 <?php 
+
+set_error_handler('exceptions_error_handler');
+
+function exceptions_error_handler($severity, $message, $filename, $lineno) {
+  if (error_reporting() == 0) {
+    return;
+  }
+  if (error_reporting() & $severity) {
+    throw new ErrorException($message, 0, $severity, $filename, $lineno);
+  }
+}
+
 class MDB_env{
 	protected $mdbEnvPtr = null;
 
